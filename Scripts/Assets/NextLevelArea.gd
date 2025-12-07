cb9c4894cbb93fdfac6ac17c13fd0a46afa7058e
Hery <Hery.rafalimanana5@gmail.com>
2025-12-07 10:34:06 +0100

    Correction Collision

--- Scripts/Assets/NextLevelArea.gd
+++ Scripts/Assets/NextLevelArea.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var NextLevel : String = ""
 
 func _on_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("Player"):
+		print("Collision")
 		if NextLevel != "":
 			get_tree().change_scene_to_file(NextLevel)
