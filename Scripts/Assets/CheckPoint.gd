835090c032463224db76af1d987a86408a44d929
Hery <Hery.rafalimanana5@gmail.com>
2025-12-07 11:49:31 +0100

    Ajout Background

--- Scripts/Assets/CheckPoint.gd
+++ Scripts/Assets/CheckPoint.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
+var IsChecked : bool = false
+@onready var Anim : AnimationPlayer = $AnimationPlayer
+
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		IsChecked = true
 		body.LastCheckPoint = body.global_position
 		print("Collision")
+
+func _process(delta: float) -> void:
+	if IsChecked:
+		Anim.play("Flag")
+		await get_tree().create_timer(0.5).timeout
+		Anim.seek(0.5)
+	else:
+		return
